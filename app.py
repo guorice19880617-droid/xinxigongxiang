f76bfd1c0f02b2f3aabe63a61799fb5f59f1d349
guorice19880617-droid <guorice19880617@gmail.com>
2026-03-17 08:13:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -64,4 +64,5 @@ def admin():
     return render_template("admin.html", data=data)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
